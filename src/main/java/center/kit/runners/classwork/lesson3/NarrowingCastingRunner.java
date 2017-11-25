fcdbe298ae316e3831f5182a79e16b614fd7507c
gybrienko <a.gybrienko@gmail.com>
2017-11-25 13:20:13 +0200

    [lesson3] NarrowingCasting

---
+++ src/main/java/center/kit/runners/classwork/lesson3/NarrowingCastingRunner.java
@@ -0,0 +1,11 @@
+package center.kit.runners.classwork.lesson3;
+
+import center.kit.app.classwork.lesson3.NarrowingCasting;
+
+public class NarrowingCastingRunner {
+    public static void main(String[] args) {
+        NarrowingCasting narrowingCasting =
+                new NarrowingCasting();
+        narrowingCasting.narrowing();
+    }
+}
