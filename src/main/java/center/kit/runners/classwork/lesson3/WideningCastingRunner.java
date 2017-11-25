dfb47f0802c8d4313c5aedc82b91797a5503c093
gybrienko <a.gybrienko@gmail.com>
2017-11-25 13:20:46 +0200

    [lesson3] WideningCasting

---
+++ src/main/java/center/kit/runners/classwork/lesson3/WideningCastingRunner.java
@@ -0,0 +1,13 @@
+package center.kit.runners.classwork.lesson3;
+
+import center.kit.app.classwork.lesson3.WideningCasting;
+
+public class WideningCastingRunner {
+
+    public static void main(String[] args) {
+        WideningCasting wideningCasting =
+                new WideningCasting();
+        wideningCasting.widening();
+
+    }
+}
